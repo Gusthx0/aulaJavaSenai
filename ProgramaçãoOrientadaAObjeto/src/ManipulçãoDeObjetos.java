48952df86f41a2465da423b7e874d8b5ea2e7d30
aluno <aluno@DSN1251017056>
2022-04-04 12:27:20 -0300

    =

---
+++ ProgramaçãoOrientadaAObjeto/src/ManipulçãoDeObjetos.java
@@ -0,0 +1,24 @@
+
+public class ManipulçãoDeObjetos {
+
+	public static void main(String[] args) {
+		
+		pessoa pessoa1 = new pessoa (); 
+		
+		
+		pessoa1.nome = "Gustavo";
+		pessoa1.numfigurinha = 1;
+		
+
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numfigurinha);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
