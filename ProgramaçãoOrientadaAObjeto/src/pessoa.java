3da5fc3ba4ac1c48945bcb8a0f69e39fb8051cd0
aluno <aluno@DSN1251017041>
2022-04-14 11:29:54 -0300

    commit quarto

--- ProgramaçãoOrientadaAObjeto/src/pessoa.java
+++ ProgramaçãoOrientadaAObjeto/src/pessoa.java
@@ -1,16 +1,35 @@
 
 public class pessoa {
 	String nome;
-	int numfigurinha;
+	int numfigurinhas;
 	
-	void receber (int numfigurinha) {
-		this.numfigurinha += numfigurinha;
+	void receber (int numfig) {						//(xxxx)-parametros/o que precisa para funcionar o meu codigo
+		
+		this.numfigurinhas += numfig;
 		
 	}
 	
-	void entregar () {
-	
+	    boolean dar (int numfig, pessoa pessoa) {
 		
+	    	
+	    	
+		if  (this.numfigurinhas > numfig) {	
+		System.out.println("quantidade insuficiente de figurinhas");
+	    return false;
+	   
+	    
 	}
-
-}
+			 
+		else {
+		this.numfigurinhas -= numfig;
+				
+		pessoa.receber(numfig);
+		return true;
+	}
+	}	
+	}
+	
+	 
+		
+		
+	
