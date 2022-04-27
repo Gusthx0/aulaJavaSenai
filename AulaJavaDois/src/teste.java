8a6203db4f4593d40c0b4fea9ddc6736b1eeb537
aluno <aluno@DSN1251017057>
2022-04-27 11:34:14 -0300

    aaa

--- AulaJavaDois/src/teste.java
+++ AulaJavaDois/src/teste.java
@@ -1,8 +1,160 @@
 
 public abstract class teste {
-
+ import java.util.Scanner;
 	public static void main(String[] args) {
-		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		
+		//int x = 100;    
+		
+				//double y = 200.3;
+				
+				//short z = (short)100000;
+				
+				//int a = 0;
+				//double b = 0;
+				
+				//a = (int) y;
+				//b = x;
+				
+				//System.out.println(a);
+				//System.out.println(b);
+				
+				//int IdadePessoa = 14;
+				
+				//if (IdadePessoa < 18) {
+					//System.out.println("você não pode beber");
+					
+					
+				//}
+				
+				//else if (IdadePessoa < 60) {
+					//System.out.println("bora tomar uma");
+					
+					
+				//}
+				
+				//else {
+					//System.out.println("quanto é sua aposentadoria ?");
+					
+				//}
+				
+				
+				
+					//System.out.println("você tem " + IdadePessoa + " anos");
+				//casting 
+				
+				
+//				int num = 3;
+//				
+//				switch (num) { //dentro das chaves as variaveis,depois a constante "1", usar sempre o ponto e virgula para o break,default para um "outro caso"
+//				
+//				case 1 :
+//					System.out.println("domingo");
+//					
+//				break;
+		//
+//				case 2 :
+//					System.out.println("segunda");
+//				break;
+//				
+//				case 3 :
+//					System.out.println("terça");
+//				break;
+//				
+//				case 4 :
+//					System.out.println("quarta");
+//				break;
+//				
+//				case 5 :
+//					System.out.println("quinta");
+//				break;
+//				
+//				case 6 :
+//					System.out.println("sexta");
+//				break;
+//				
+//				case 7 :
+//					System.out.println("sabado");
+//				break;
+//				
+//				default : 
+//					System.out.println("esse numero não representa um dia da semana");
+//				
+//				}
+				
+//				char caractere = 'b';
+//				
+//				switch (caractere) {
+//				case 'a':
+//				System.out.println("seu caracter é A");		
+//				break;
+//				
+//				
+//				
+//				case 'b':
+//				System.out.println("seu caracter é B");		
+//				break;
+//				
+//				
+//				default :
+//					System.out.println("valor não encontrado");
+//				}
+				
+//				String farol = "aleatorio";
+//				
+//				switch (farol) {
+//				
+//				
+//				case "vermelho" :
+//				System.out.println("PARE");
+//				break;
+//				
+//				case "verde" :
+//				System.out.println("SIGA");
+//				break;
+//				
+//				case "amarelo" :
+//				System.out.println("ATENÇÃO");
+//				break;
+//				
+//				default :
+//					System.out.println("valor invalido");
+//				
+//				}	
+				
+//				int x=0;
+//				while(x<10) { //testa primeiro
+//					x=x+1;
+//					System.out.println(x);
+//				}
+//				
+//				int x=0;
+//				do {				//executa primeiro
+//					System.out.println(x);
+//					x=x+1;
+//				}while(x<10);
+//				
+				
+//				for (int x=70; x <1000; x+=4) {      //a var. so existe aqui dentro e não pode ser chamada por fora do codigo,++add 1 ao number,+=2
+//					System.out.println(x);
+//				
+//				
+//				}
+		//
+		
+		
+		
+			Scanner entrada = new Scanner (System.in);
+			System.out.println("digite o numero");
+			int y = entrada.nextInt();
+			
+			for(int x=0; x,10;.x++) {
+				System.out.println(y + "x" + x + "=" + x*y);
+			}
+		
+		
+		
+		
+		
 		
 
 	}
