466c811f2950a293b775b42824bb20a7f0a1df8c
aluno <aluno@DSN1251017057>
2022-04-27 11:39:03 -0300

    e

---
+++ ModificadorAcesso/src/main.java
@@ -0,0 +1,60 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		ContaBancaria conta1 = new ContaBancaria(); //usar a letra maiscula
+	
+//			conta1.ativo = true ;
+//			
+//				conta1.numConta = 222;
+//			
+//			conta1.saldo = 150.60;
+//			
+//
+//			System.out.println(conta1.ativo);
+//			
+//		System.out.println(conta1.numConta);
+//		
+//	System.out.println(conta1.saldo);	
+//		
+		conta1.receber(185.70);
+		conta1.dar(50.75);
+		System.out.println(conta1.saldo());
+		
+		ContaBancaria conta2 = new ContaBancaria();
+		System.out.println(conta2.saldo());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
