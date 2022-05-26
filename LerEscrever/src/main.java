565544df0ebbfbc97d731b690c646cd970db61a5
aluno <aluno@DSN1251017050>
2022-05-26 11:47:09 -0300

    aaaa

---
+++ LerEscrever/src/main.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
