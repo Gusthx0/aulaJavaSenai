70f0f98619d11c6b5a244268f8603fe1476267d1
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-07 17:55:45 -0300

    comentado
    com tudo que eu me recordo sobre interface

--- Interface/src/Aplicacao.java
+++ Interface/src/Aplicacao.java
@@ -1,27 +1,30 @@
 
 public class Aplicacao {
 
-	public static void main(String[] args) { //para fazer a "rota" � necessario a var. rota e um automovel
+	public static void main(String[] args) { //para fazer a "rota" é necessario a var. rota e um automovel
 
-		Rota rota = new Rota();
+		Rota rota = new Rota(); //tudo que tem "tipo nome = new tipo< ESTA INSTANCIANDO (criando) um objeto
 		Ferrari ferrari = new Ferrari();
 		rota.ir(ferrari);
 		
 		Fusca fusca = new Fusca();
-		rota.ir(fusca);
+		rota.ir(fusca);//aqui,criamos um objeto do tipo fusca e acessamos de rota e o metodo ir que esta em rota e tem atributos de automovel
 		System.out.println();
 		
 		Automovel automovel = new Ferrari();
-		rota.ir(automovel);
+		rota.ir(automovel); //aqui fazemos o memsmo de fusca só que com ferari
+		
 		automovel = new Fusca();
 		rota.ir(automovel);
 		
-		Vendedor vendedor = new Vendedor();
+		Vendedor vendedor = new Vendedor(); //aqui estamos criando um objeto vendedor nessa clase, para acessar o preço de apartamento (obj. criado abaixo) e ferrari criado acima
 		Apartamento apartamento = new Apartamento(); 
 		
 		vendedor.mostrarPreco(apartamento);
 		vendedor.mostrarPreco(ferrari);
 		
+		//vendedor tem como parametro** itemcaro e pode acessar o valor atraves de getpreco,que é uma variavel* ou um getter* presente em ItemCaro(no caso a interface)
+		//(tambem não tenho certeza se é um getter mesmo*)(**não tenho certeza se seria essa a nomeclatura correta para o metodo MostarPreco de vendedor)
 		 
 		
 	}
