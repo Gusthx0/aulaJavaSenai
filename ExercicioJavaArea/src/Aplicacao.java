99d33ec09692615d777069aa17e4f15aba3c295d
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-10 19:17:10 -0300

    comentado

--- ExercicioJavaArea/src/Aplicacao.java
+++ ExercicioJavaArea/src/Aplicacao.java
@@ -3,14 +3,14 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Triangulo triangulo = new Triangulo();
+		Triangulo triangulo = new Triangulo(); //instanciando objetos do tipo evidente (triangulo,quadrado e retangulo)
 		Retangulo retangulo = new Retangulo();
 		Quadrado quadrado = new Quadrado();
 		
 		quadrado.a = 3;
 		triangulo.h = 9;
 		triangulo.a = 3;
-		retangulo.alt = 5;
+		retangulo.alt = 5; //acessando atraves dos objetos as variaveis e declarando qual o seu valor
 		retangulo.lar = 10;
 		
 	}
