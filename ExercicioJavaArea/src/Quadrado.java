8eb688b2fb9d4f2f3cae422cf032d0fd1be48314
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-10 19:21:01 -0300

    comentado

--- ExercicioJavaArea/src/Quadrado.java
+++ ExercicioJavaArea/src/Quadrado.java
@@ -5,7 +5,7 @@ public class Quadrado implements AreaCalculavel {
 	
 	@Override
 	public void calcularArea() {
-		System.out.println("A área do quadrado é " + a * 2);
+		System.out.println("A Ã¡rea do quadrado Ã© " + a * 2);  //aqui usamos o metodo implemnetado para personalizar e realizar o calculo de acordo a forma
 	}
 
 }
