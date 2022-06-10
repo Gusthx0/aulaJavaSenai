e83fcac9aff76416690d15f68dfbc6135f869801
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-10 20:32:41 -0300

    comentado

--- PolimorfismoBebidas/src/Preparador.java
+++ PolimorfismoBebidas/src/Preparador.java
@@ -1,14 +1,14 @@
 
 public class Preparador {
 
-	public void preparaBebida(bebida bebida) {
+	public void preparaBebida(bebida bebida) { //aqui o metodo generico que vai servi para todas as bebidas 
 		
-		System.out.println("PREPARANDO A BEBIDA " + bebida.getName());
+		System.out.println("PREPARANDO A BEBIDA " + bebida.getName()); //como elel recebe bebida como parametro todas podem acessa-lo
 		
-		bebida.preparar();
+		bebida.preparar(); //como sera acessado
 		
 		if (bebida.isAquecer()) {
-			System.out.println("AQUECENDO A BEBIDA " + bebida.getName());
+			System.out.println("AQUECENDO A BEBIDA " + bebida.getName()); //aqui um if pra caso a bebida seja quente como é o caso do leite
 			
 		}
 		
