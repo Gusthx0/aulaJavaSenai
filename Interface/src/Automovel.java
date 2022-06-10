c8e270be6f40221a2d191d2b0ea0a40a9ba4b142
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-10 20:37:33 -0300

    comentado

--- Interface/src/Automovel.java
+++ Interface/src/Automovel.java
@@ -1,5 +1,5 @@
 
-public interface Automovel {
+public interface Automovel { //uma inteface obriga as classe que implementam dela a ter seus metodos e não se pode criar atributos nela
 	void viraEsquerda ();
 	void viraDireita ();
 	void acelerar ();
