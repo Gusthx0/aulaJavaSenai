d025df9c66f6dcd8779e36a581f474d041aa42bb
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-10 20:34:49 -0300

    comentado

--- PolimorfismoBebidas/src/Aplicacao.java
+++ PolimorfismoBebidas/src/Aplicacao.java
@@ -3,12 +3,12 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Preparador preparador = new Preparador();
+		Preparador preparador = new Preparador(); //um objeto sendo instanciado para acessar um mesmo metodo dele
 		
-		LeiteQuente leiteQuente = new LeiteQuente();
+		LeiteQuente leiteQuente = new LeiteQuente(); //instancia um objeto do tipo leite que vai usar prteparador para acessar o metod que vai "preparar' a bebida
 		preparador.preparaBebida(leiteQuente);
 		
-		Refrigerante refrigerante = new Refrigerante();
+		Refrigerante refrigerante = new Refrigerante();//faqz o mesmo que o de cima
 		preparador.preparaBebida(refrigerante);
 		
 		
