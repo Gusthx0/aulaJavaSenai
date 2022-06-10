6f5fffebfc66da74d2bb71436b7b963eda71cbca
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-10 19:46:25 -0300

    comentado

--- EscreverTXT/src/Main.java
+++ EscreverTXT/src/Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
-import java.io.BufferedWriter;
+import java.io.BufferedWriter; //aqui importamos os metodos que leem e escrevem da classe do java
 import java.io.FileWriter;
-import java.util.ArrayList;
+import java.util.ArrayList;//aqui importamos o array e abaixo o list
 import java.util.List;
 
 
@@ -11,27 +11,27 @@ public class Main {
 		
 //		try(BufferedWriter escrever = new BufferedWriter (new FileWriter("Entrada.TXT"))){
 
-		Mouse mouse0 = new Mouse("true","2","médio");
+		Mouse mouse0 = new Mouse("true","2","médio"); //aqui nos instanciamos objetos do tipo mouse e declaramos os seus determinados atributos
 		Mouse mouse1 = new Mouse("true","2","médio");
 		Mouse mouse2 = new Mouse("true","2","médio");
 		
-		List<Mouse> listademouses = new ArrayList<Mouse>();
+		List<Mouse> listademouses = new ArrayList<Mouse>(); //aqui criamos um arraylist chamado lista de mouses que vai receber objetos do tipo mouse
 		
 		listademouses.add(mouse0);
 		listademouses.add(mouse1);
-		listademouses.add(mouse2);
+		listademouses.add(mouse2); //aqui adicionamos objetos mouse na arraylist
 		
-		for (Mouse mouse : listademouses) {
+		for (Mouse mouse : listademouses) { //aqui fazemos o  enhanced for, que de forema simples vai prcorrer o arraylist procurando objetos do tipo e depois mostrando no syso abaixo alem de escrever o numero de vezes que o codigo necessita
 			System.out.println(mouse);
 		
 		}
 		
-		try(BufferedWriter escrever = new BufferedWriter (new FileWriter("Entrada.TXT"))){
+		try(BufferedWriter escrever = new BufferedWriter (new FileWriter("Entrada.TXT"))){ //o bufferedwrite vai escrever a lista de mouses acima e depois o filewriter vai colocar em um arquivo TXT
 		 
-		for(Mouse mouse : listademouses)
+		for(Mouse mouse : listademouses)  //para cada variavel do tipo mouse ele vai fazer o seguinte comando
 			
 			
-			escrever.write (mouse.toString());
+			escrever.write (mouse.toString());  //aqui mouse esta acessando o tostring que sera o propio texto
 		
 		}
 			
