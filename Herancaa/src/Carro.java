acb600259a9714e072adb5baafecc705ddfe734e
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-11 23:39:52 -0300

    comentado

--- Herancaa/src/Carro.java
+++ Herancaa/src/Carro.java
@@ -3,7 +3,7 @@ public class Carro extends Veiculo{
 	 
 	private boolean quatroPortas;
     
-	
+	//de forma bem basica, aqui nos extendemos a classe e pegamos um metodo que seria "obrigatorio' para q	uem extender dela,personalçizamos esse metodo e adicionamos um outro que seria o das quatro portas
 
 	@Override
 	public void imprimir() {
