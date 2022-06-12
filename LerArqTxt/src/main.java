44b26f5055c3dcf33e840e83c4f358550fdadae4
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-12 12:50:28 -0300

    comentado

--- LerArqTxt/src/main.java
+++ LerArqTxt/src/main.java
@@ -7,11 +7,11 @@ public class main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException{
 		
-		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.TXT"))){
+		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.TXT"))){  //esta lendo e colocando em um arq txt
 			String line;
 			String string ="";
 			
-			while ((line = reader.readLine())!= null) {
+			while ((line = reader.readLine())!= null) {   //enquanto line for diferente de vazio,e line recebe o arq de leitura,faca
 				string += line + "\n";
 				
 				
