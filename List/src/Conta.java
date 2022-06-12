da284f5be83f75d2750b4b3526ed322082c3c739
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-12 12:48:30 -0300

    comentado

--- List/src/Conta.java
+++ List/src/Conta.java
@@ -3,12 +3,13 @@ public class Conta {
 	
 	String nome;
 	double valor;
-	public Conta(String nome, double valor) {
+	
+	public Conta(String nome, double valor) {  //para acessar posteriormente esses atributos aqui de conta
 		super();
 		this.nome = nome;
 		this.valor = valor;
 	}
-	@Override
+	@Override			//passa para stringo o que esta sendo digitado aqui
 	public String toString() {
 		return "Conta [valor=" + valor + "]";
 	}
