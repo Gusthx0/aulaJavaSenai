f0543a5362826ebc0e5241f185154a869ed3f5b9
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-11 23:50:50 -0300

    comentado

--- Lampada/src/main.java
+++ Lampada/src/main.java
@@ -2,22 +2,16 @@
 public class main {
 
 	public static void main(String[] args) {
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		//primeiro programa que fizemos
+		//siso mostra o texto na tela
+			System.out.print ("pegue a lampada nova")
+			System.out.print ("pegue a escada")
+			System.out.print ("suba a escada")
+			System.out.print ("com cuidado,retire a lampada antiga")
+			System.out.print ("pegue a lampada nova e encaixe no bocal")
+				System.out.print ("teste a nova  lampada")
+				System.out.print ("se tudo estiver ocorrendo bem,sem avarias,paraben!!")
+				System.out.print ("se não,procure o procom e boa sorte KKKKKKK")
 
 	}
 
