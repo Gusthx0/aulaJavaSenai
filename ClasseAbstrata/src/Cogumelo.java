ca1556cf761532098e10cbd943921b93e15f74ef
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-11 23:57:11 -0300

    comentado

--- ClasseAbstrata/src/Cogumelo.java
+++ ClasseAbstrata/src/Cogumelo.java
@@ -1,6 +1,7 @@
 
 public class Cogumelo extends Item {
-
+//nem sei mais professor
+	//brincadeira esse é um metodo herdado da super classe
 	@Override
 	public void pegar() {
 		System.out.println("Pegou o Cogumelo +100 pontos");
