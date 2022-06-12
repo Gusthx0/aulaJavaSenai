b8e852e5c712a5a3ff4527612e0ff7e550e9b1cd
Gusthx0 <102963719+Gusthx0@users.noreply.github.com>
2022-06-11 23:52:14 -0300

    comentado

--- ConstrutorDeHorario/src/Aplicacao.java
+++ ConstrutorDeHorario/src/Aplicacao.java
@@ -3,7 +3,7 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 	
-		Data d1 = new Data(10, 03, 2000, 0, 30, 10);
+		Data d1 = new Data(10, 03, 2000, 0, 30, 10); //instanciando e mostrando na tela
 		d1.imprimir(Data.FORMATO_12H);
 		d1.imprimir(Data.FORMATO_24H);
 
